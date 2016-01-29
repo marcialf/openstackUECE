5b09ad49d204fd6a120970dc7713f8c9d7ec7345
stack <nashjunior>
2016-01-28 22:21:13 -0300

    novo arquivo keystone

--- controller-node/05_openstack_keystone.sh
+++ controller-node/05_openstack_keystone.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 ADMIN_TOKEN="406088b3e19f7131ebd4"
 MARIADB_PASSWORD="openstack"
 KEYSTONE_DB_PASSWORD="openstack"
@@ -12,12 +10,12 @@ function assert_superuser {
 	[[ "$(id -u)" != "0" ]] && echo "You need to be 'root' dude." 1>&2 && exit 1
 }
 
-function create_keystone_database {	
-	#cp "/vagrant/templates/keystone.sql" "/vagrant/temp/keystone.sql"	
-	#sed -i'' -e"s/KEYSTONE_DBPASS/$KEYSTONE_DB_PASSWORD/" "/vagrant/temp/keystone.sql"
-	mysql -u root "-p${MARIADB_PASSWORD}" < "keystone.sql"
-}
 
+function create_keystone_database
+{
+	mysql -u root "-p${MARIADB_PASSWORD}" < "/home/openstack/Documentos/openstackUECE/sql/keystone.sql"
+	
+}
 
 function disable_keystone_autostart {
 	echo "manual" > /etc/init/keystone.override
@@ -28,30 +26,34 @@ function install_keystone_packages {
 	apt-get install -y keystone python-openstackclient apache2 libapache2-mod-wsgi memcached python-memcache
 }
 
-
-function configure_keystone {	
-	#cp "/vagrant/templates/keystone.conf" "/vagrant/temp/keystone.conf"	
-	#sed -i'' -e"s/ADMIN_TOKEN_PLACE/admin_token = $ADMIN_TOKEN/" "/vagrant/temp/keystone.conf"
-	#sed -i'' -e"s~DB_CONNECTION_PLACE~connection = mysql://keystone:$KEYSTONE_DB_PASSWORD@controller/keystone~"	"/vagrant/temp/keystone.conf"
-	rm "/etc/keystone/keystone.conf"
-	cp "/home/stack/keystone.conf" "/etc/keystone/keystone.conf"
+function configure_keystone
+{
+	cp "/home/openstack/Documentos/keystone.conf" "/etc/keystone/keystone.conf"	
 }
 
-
-function init_keystone_database {
+function connect_database
+{
 	su -s /bin/sh -c "keystone-manage db_sync" keystone
 }
 
-function configure_apache {
-	echo "ServerName $(hostname)" >> "/etc/apache2/apache2.conf"
-	cp "/home/stack/wsgi-keystone.conf" "/etc/apache2/sites-available/wsgi-keystone.conf"
-	chmod 755 /etc/apache2/sites-available/wsgi-keystone.conf
+function configure_apache2
+{
+	cp "/home/openstack/Documentos/apache2.conf" "/etc/apache2/apache2.conf"
+
+	cp "/home/openstack/Documentos/wsgi-keystone.conf" "/etc/apache2/sites-available"
+
 	ln -s "/etc/apache2/sites-available/wsgi-keystone.conf" "/etc/apache2/sites-enabled"
+
 	mkdir -p "/var/www/cgi-bin/keystone"
+
 	curl "http://git.openstack.org/cgit/openstack/keystone/plain/httpd/keystone.py?h=stable/kilo" | tee "/var/www/cgi-bin/keystone/main" "/var/www/cgi-bin/keystone/admin"
+
 	chown -R keystone:keystone "/var/www/cgi-bin/keystone"
+
 	chmod 755 /var/www/cgi-bin/keystone/*
+
 	service apache2 restart
+
 	remove_if_exists "/var/lib/keystone/keystone.db"
 }
 
@@ -59,7 +61,8 @@ function remove_if_exists {
 	[[ -f "$1" ]] && rm -f "$1"
 }
 
-function create_project_users_and_roles {
+function create_auth_user_and_role
+{
 	export OS_SERVICE_TOKEN=$ADMIN_TOKEN	
 	export OS_SERVICE_ENDPOINT=http://controller:35357/v2.0
 	
@@ -75,13 +78,15 @@ function create_project_users_and_roles {
 
 	keystone user-create --name demo --tenant demo --pass $USER_DEMO_PASSWORD --email $EMAIL_DEMO
 
-	keystone tenant-create --name service --description "Service Tenant"	 
+	keystone tenant-create --name service --description "Service Tenant"
 }
 
-
-function create_service_and_api_entrypoint {
+function create_service_and_api_entrypoint
+{
 	export OS_SERVICE_TOKEN=$ADMIN_TOKEN	
+
 	export OS_SERVICE_ENDPOINT=http://controller:35357/v2.0
+
 	keystone service-create --name keystone --type identity \
 		--description "OpenStack Identity"	
 
@@ -92,7 +97,6 @@ function create_service_and_api_entrypoint {
 		--adminurl http://controller:35357/v2.0 \
 		--region regionOne
 }
-
 
 function verify_operation
 {
@@ -120,16 +124,16 @@ function verify_operation
 
 
 
-function main
+function main 
 {
 	assert_superuser
 #	create_keystone_database
-#	disable_keystone_autostart	
+#	disable_keystone_autostart
 #	install_keystone_packages
 #	configure_keystone
-#	init_keystone_database
-	configure_apache
-	create_project_users_and_roles
+#	connect_database
+#	configure_apache2
+	create_auth_user_and_role
 	create_service_and_api_entrypoint
 	verify_operation
 }
