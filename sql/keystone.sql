49271123b89844ee7a410e99948dd8f7f56db1ee
stack <nashjunior>
2016-01-29 00:30:29 -0300

    arquvos sql

--- sql/keystone.sql
+++ sql/keystone.sql
@@ -1,3 +1,5 @@
+CREATE database keystone;
+
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'openstack';
 
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'openstack';
