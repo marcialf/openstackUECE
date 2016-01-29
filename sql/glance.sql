adfc2c2b6648fe9223b052a9f2953ac23e7a9fef
stack <nashjunior>
2016-01-28 23:59:23 -0300

    glance sql script

--- sql/glance.sql
+++ sql/glance.sql
@@ -1,7 +1,7 @@
 CREATE database glance;
 
-GRANT ALL PRIVILEGES ON glance.* TO 'keystone'@'localhost' IDENTIFIED BY 'openstack';
+GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' IDENTIFIED BY 'openstack';
 
-GRANT ALL PRIVILEGES ON glance.* TO 'keystone'@'%' IDENTIFIED BY 'openstack';
+GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY 'openstack';
 
 
