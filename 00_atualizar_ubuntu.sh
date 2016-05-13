9e61aa68b6ad84b30be83e953e160c36f658081c
nashjunior <nashjuniorjj@gmail.com>
2016-05-13 12:01:13 -0300

    arquivos de configuracao da rede

--- 00_atualizar_ubuntu.sh
+++ 00_atualizar_ubuntu.sh
@@ -9,6 +9,7 @@ function edit_network
 	cp "/home/openstack/Documentos/openstackUECE/controller-node/interfaces" "/etc/network/interfaces"
 	cp "/home/stack/Documentos/openstackUECE/compute-node/interfaces" "/etc/network/interfaces"
 	cp "/home/cinder/Documentos/openstackUECE/storage-node/interfaces" "/etc/network/interfaces"
+	cp "/home/swift/Documentos/openstackUECE/object-storage-node/node-1/interfaces" "/etc/network/interfaces"
 
 
 }
@@ -18,6 +19,7 @@ function copy_hosts
 	cp "/home/openstack/Documentos/openstackUECE/controller-node/hosts" "/etc/hosts"
 	cp "/home/stack/Documentos/openstackUECE/compute-node/hosts" "/etc/hosts"
 	cp "/home/cinder/Documentos/openstackUECE/storage-node/hosts" "/etc/hosts"
+	cp "/home/swift/Documentos/openstackUECE/object-storage-node/node-1/hosts" "/etc/hosts"
 
 }
 
