36ddb4efdb8a4c6d10903dc797285ff9d85144b4
nashjunior <nashjuniorjj@gmail.com>
2016-05-16 10:22:37 -0300

    arquivo de configuracao do swift

--- object-storage-node/node-2/02_1_configure_prerequisites.sh
+++ object-storage-node/node-2/02_1_configure_prerequisites.sh
@@ -6,18 +6,18 @@ function assert_superuser {
 
 function configure_swift
 {
-        cp "/home/swift/Documentos/openstackUECE/conf/fstab" "/etc/fstab"
-	cp "/home/swift/Documentos/openstackUECE/conf/rsyncd2.conf" "/etc/rsyncd.conf"
-        cp "/home/swift/Documentos/openstackUECE/conf/rsync" "/etc/default/rsync"
+        cp "/home/swift2/Documentos/openstackUECE/object-storage-node/conf/fstab" "/etc/fstab"
+	cp "/home/swift2/Documentos/openstackUECE/object-storage-node/conf/rsyncd2.conf" "/etc/rsyncd.conf"
+        cp "/home/swift2/Documentos/openstackUECE/object-storage-node/conf/rsync" "/etc/default/rsync"
 }
 
 
 function main
 {
         assert_superuser
-        apt-get instal -y install xfsprogs rsync
-        mkfs.xfs /dev/sdb1
-        mkfs.xfs /dev/sdc1
+#        apt-get install -y xfsprogs rsync
+#        mkfs.xfs /dev/sdb1
+#        mkfs.xfs /dev/sdc1
 
         mkdir -p /srv/node/sdb1
         mkdir -p /srv/node/sdc1
