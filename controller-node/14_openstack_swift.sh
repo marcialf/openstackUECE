e91ab856672c9c012f814c4e18dd61a866bd89d9
nashjunior <nashjuniorjj@gmail.com>
2016-05-16 09:02:18 -0300

    script de instalacao do swift

--- controller-node/14_openstack_swift.sh
+++ controller-node/14_openstack_swift.sh
@@ -7,23 +7,30 @@ SWIFT_DB_PASSWORD="openstack"
 KEYSTONE_USER_SWIFT_PASSWORD="openstack"
 EMAIL_SWIFT="swift@example.com"
 
+
+function assert_superuser {
+	[[ "$(id -u)" != "0" ]] && echo "You need to be 'root' dude." 1>&2 && exit 1
+}
+
 function register_in_keystone
 {
 	source "/home/openstack/Documentos/openstackUECE/controller-node/admin-demo/admin-openrc.sh"
-	openstack user create --password-prompt swift $KEYSTONE_USER_SWIFT_PASSWORD$
-	openstack role add --project service --user swift admin
-	openstack service create --name swift --description "OpenStack Object Storage" object-store
-	openstack endpoint create --publicurl 'http://controller:8080/v1/AUTH_%(tenant_id)s' \
-		--internalurl 'http://controller:8080/v1/AUTH_%(tenant_id)s' \
-		--adminurl http://controller:8080 \
-		--region RegionOne \
-		object-store
+	keystone user-create --name swift --pass $KEYSTONE_USER_SWIFT_PASSWORD$
+	keystone user-role-add --user swift --tenant service --role admin
+	keystone service-create --name swift --type object-store --description "OpenStack Object Storage"
+	keystone endpoint-create \
+	  --service-id $(keystone service-list | awk '/ object-store / {print $2}') \
+	  --publicurl 'http://controller:8080/v1/AUTH_%(tenant_id)s' \
+	  --internalurl 'http://controller:8080/v1/AUTH_%(tenant_id)s' \
+	  --adminurl http://controller:8080 \
+	  --region regionOne
 }
 
 function install_packages
 {
-	apt-get install swift swift-proxy python-swiftclient python-keystoneclient \
+	apt-get install swift python-swiftclient python-keystoneclient \
 		python-keystonemiddleware memcached
+	dpkg dpkg -i "/home/openstack/Documentos/openstackUECE/swift-proxy_2.2.2-0ubuntu1.3~cloud0_all.deb"
 }
 
 function configure_swift
@@ -33,13 +40,14 @@ function configure_swift
 
 function main
 {
-	register_in_keystone
+	assert_superuser
+#	register_in_keystone
 	install_packages
 	mkdir /etc/swift
 	curl -o /etc/swift/proxy-server.conf \
 		https://git.openstack.org/cgit/openstack/swift/plain/etc/proxy-server.conf-sample?h=stable/kilo
 	configure_swift
-	
+
 
 }
 main
